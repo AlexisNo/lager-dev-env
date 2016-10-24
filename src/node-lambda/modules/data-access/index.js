1b5b48014c3ef0532b1536ba64a0f9c93bc93cc4
AlexisNo <alexis@henaut.net>
2016-10-24 20:37:25 -0200

    Update to latest version of plugins

--- src/node-lambda/modules/data-access/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-console.log('loading data-access package');
-
-const log = require('log');
-
-module.exports = 'This node package should implement data access';
--- src/node-lambda/modules/log/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-console.log('loading log package');
-
-module.exports = 'This node package should implement a logging system';
